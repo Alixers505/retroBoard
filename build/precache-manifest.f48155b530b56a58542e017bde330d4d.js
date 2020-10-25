7422108f0c51384b75e9a3246271a2a71b6d194e
Alysha Buselt <alyshabuselt@gmail.com>
2020-10-25 17:32:51 -0400

    Added build

---
+++ build/precache-manifest.f48155b530b56a58542e017bde330d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db295fabceb41ee33a909820735c52e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b888d7ab9f09395ae5",
+    "url": "/static/css/main.acbdef94.chunk.css"
+  },
+  {
+    "revision": "4a46ef59bd01ba17f32c",
+    "url": "/static/js/2.c77347b5.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.c77347b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b888d7ab9f09395ae5",
+    "url": "/static/js/main.e4409a45.chunk.js"
+  },
+  {
+    "revision": "36d3c40e018011e5135a",
+    "url": "/static/js/runtime-main.70e72e38.js"
+  }
+]);
